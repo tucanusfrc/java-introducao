3d8a8aef687aa1ebf26cf54996b05a5cb9fc3588
tucanusfrc <tucanusnffrc@gmail.com>
2023-09-14 20:16:30 -0300

    java atualizado

--- teste-bancada/codigo-victor-2023-java/src/main/java/frc/robot/Robot.java
+++ teste-bancada/codigo-victor-2023-java/src/main/java/frc/robot/Robot.java
@@ -4,13 +4,11 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -24,42 +22,40 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends TimedRobot {
 
-  CANSparkMax m_garra = new CANSparkMax(7, MotorType.kBrushless);
-  
-  CANSparkMax m_leftFrontal = new CANSparkMax(4, MotorType.kBrushless);
-  CANSparkMax m_leftBack = new CANSparkMax(1, MotorType.kBrushless);
-  CANSparkMax m_rightFrontal = new CANSparkMax(2, MotorType.kBrushless);
-  CANSparkMax m_rightBack = new CANSparkMax(3, MotorType.kBrushless);
+  WPI_VictorSPX m_garra = new WPI_VictorSPX(7);
 
-  CANSparkMax m_estica = new CANSparkMax(5, MotorType.kBrushless);
-  CANSparkMax m_elevador = new CANSparkMax(6, MotorType.kBrushless);
+  WPI_VictorSPX m_leftFrontal = new WPI_VictorSPX(4);
+  WPI_VictorSPX m_leftBack = new WPI_VictorSPX(1);
+  WPI_VictorSPX m_rightFrontal = new WPI_VictorSPX(2);
+  WPI_VictorSPX m_rightBack = new WPI_VictorSPX(3);
+
+  WPI_VictorSPX m_estica = new WPI_VictorSPX(5);
+  WPI_VictorSPX m_elevador = new WPI_VictorSPX(6);
 
   //FIM DE CURSO
-  DigitalInput travaSuperior = new DigitalInput(8);
-  DigitalInput travaInferior = new DigitalInput(0);
+  DigitalInput travaSuperior = new DigitalInput(0);
+  DigitalInput travaInferior = new DigitalInput(1);
 
   MotorControllerGroup m_left = new MotorControllerGroup(m_leftBack, m_leftFrontal);
   MotorControllerGroup m_right = new MotorControllerGroup(m_rightBack, m_rightFrontal);
 
-  DifferentialDrive chassi =  new DifferentialDrive(m_left, m_right);
+  DifferentialDrive chassi = new DifferentialDrive(m_left, m_right);
 
   private XboxController m_driveStick;
   private XboxController m_garraStick;
 
-  private Timer timer = new Timer();
-
   @Override
   public void robotInit() {
-    SmartDashboard.putString("Iniciado: ", "true");
 
     m_rightBack.setInverted(true);
     m_rightFrontal.setInverted(true);
     m_leftBack.setInverted(false);
     m_leftFrontal.setInverted(false);
 
-    m_estica.setIdleMode(IdleMode.kBrake);
-    m_elevador.setIdleMode(IdleMode.kBrake);
-    m_garra.setIdleMode(IdleMode.kBrake); 
+    SmartDashboard.putBoolean("Inverted: ", m_rightFrontal.getInverted());
+    
+    m_elevador.setNeutralMode(NeutralMode.Brake);
+    m_garra.setNeutralMode(NeutralMode.Brake);
 
     m_driveStick = new XboxController(0);
     m_garraStick = new XboxController(1);
@@ -73,21 +69,10 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {
-    timer.restart();
-    timer.start();
-  }
+  public void autonomousInit() {}
 
   @Override
-  public void autonomousPeriodic() {
-    if(timer.get()<3){
-      chassi.tankDrive(0.1, 0.1);
-    } else if (timer.get()>3 && timer.get()<6){
-      chassi.tankDrive(0.2, -0.2);
-    } else{
-      chassi.stopMotor();
-    }
-  }
+  public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {}
@@ -97,24 +82,13 @@ public class Robot extends TimedRobot {
     //FIM DE CURSO
     boolean estadoFimCursoSuperior = travaSuperior.get();
     boolean estadoFimCursoInferior = travaInferior.get();
-    double v_elevador = m_garraStick.getRightY();
-    
-    if (v_elevador < 0 && !estadoFimCursoInferior){
-      m_elevador.set(v_elevador);
-    } else{
-      m_elevador.set(0);
-    }
-
-    if(v_elevador > 0 && !estadoFimCursoSuperior){
-      m_elevador.set(v_elevador);
-    } else{
-      m_elevador.set(0);
-    }
 
     //MOVIMENTACAO
     chassi.tankDrive(m_driveStick.getLeftY(), m_driveStick.getRightY());
 
     //GARRA
+    m_elevador.set(m_garraStick.getRightY()); //Sug: Limitar velocidade multiplicando por decimais.
+
     m_garra.set(m_garraStick.getRightTriggerAxis()*0.6);
     m_garra.set(-m_garraStick.getLeftTriggerAxis()*0.6);
     
